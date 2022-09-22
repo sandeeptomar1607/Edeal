0a687b04bf14000ecf39d3395613db91197cb7a5
Sandeep <tsandeep996@gmail.com>
2022-09-22 19:23:59 +0530

    commit

--- store/total.py
+++ store/total.py
@@ -78,4 +78,3 @@ def prod(request, pro):
 
 
 
-git remote set-url origin https://sandeeptomar1607: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@github.com/sandeeptomar1607/git@github.com:sandeeptomar1607/Edeal.git
